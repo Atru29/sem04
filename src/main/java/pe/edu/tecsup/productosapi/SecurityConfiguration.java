b90501584663a6cf8f02626e448615d44c270706
Atru29 <you@example.com>
2023-09-16 21:31:10 -0500

    sem04

---
+++ src/main/java/pe/edu/tecsup/productosapi/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package pe.edu.tecsup.productosapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import filters.JwtAuthenticationTokenFilter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+ @Override
+ protected void configure(HttpSecurity http) throws Exception {
+ http
+ .authorizeRequests()
+ .antMatchers("/" /*, "/**"*/).permitAll()
+ .antMatchers("/auth/login").permitAll()
+ .and().csrf().disable();
+ http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+ }
+ @Bean
+ public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+ return new JwtAuthenticationTokenFilter("/api/**");
+ }
+}
